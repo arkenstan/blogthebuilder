3729f850247cfe3622ce540061f0be25db6ceb42
thebrainmaniac <anshulwagadre@live.com>
2016-12-18 07:11:19 +0530

    Made some new change
     - Added a new directives file for creator users
     - Renamed overlay File to app
     - changes in workspace

---
+++ admin/js/app.js
@@ -0,0 +1,11 @@
+/*
+*	Created on: 9/12/2016 17:43:47
+*/
+window.onload = function(){
+  alert("Loaded");
+};
+(function(){
+
+  var app = angular.module('btbApp',['adminDirectives']);
+
+})();
--- admin/js/directives.js
+++ admin/js/directives.js
@@ -1,7 +1,21 @@
+/*
+*
+* Creator Workspace Directives will Go here
+*
+*/
+window.onload = function(){
+  alert("Loaded 2");
+};
+
 (function(){
 
   var app = angular.module('adminDirectives', []);
 
-  app.directive('', function(){});
+  app.directive('navbarDashboard', function(){
+    return {
+      restrict: 'E',
+      templateUrl: './templates/navbar_template_dashboard.html'
+    };
+  });
 
 })();
