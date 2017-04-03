80a4752d02ec3c5d2df7f4c83b1d5a6d932205cc
Anshul <anshulwagadre@live.com>
2017-04-03 19:42:07 +0530

    Posts service sample

--- includes/js/services/postServices.js
+++ includes/js/services/postServices.js
@@ -1,10 +1,31 @@
 'use strict';
 
-app.service('postService', ['$http',function($http){
-  this.get = function(){};
-  this.post = function(postDetails){
-    var $promise = $http.post('./includes/functions/posts.php?act=1', postDetails);
+app.factory('postService', function($http,$window){
+  return {
+    addPost:function(post){
+      var $promise = $http.post('./includes/functions/posts.php?act=1',post);
+      return $promise;
+    },
+    updatePost:function(activity,post){
+      var $promise,id;
+      switch (activity) {
+        case 'draft':
+          id = post.post_id;
+          $promise = $http.post('./includes/functions/posts.php?act=2',id);
+          break;
+        case 'unpublish':
+          id = post.post_id;
+          $promise = $http.post('./includes/functions/posts.php?act=3',id);
+          break;
+        default:
+          $promise = $http.post('./includes/functions/posts.php?act=4',post);
+          break;
+      }
+      return $promise;
+    },
+    deletePost:function(postID){
+      var $promise = $http.post('./includes/functions/posts.php?act=5',postID);
+      return $promise;
+    },
   };
-  this.delete = function(){};
-  this.update = function(){};
-}]);
+});
